527bd74c630b44089c3fd2638a613b9dd7144345
jiya-gandhi-108 <jiya108gandhi@gmail.com>
2026-01-03 12:24:49 +0530

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
-  const authHeader = req.headers.authorization; // "Bearer token"
+  const authHeader = req.headers.authorization; 
   if (!authHeader) {
     return res.status(401).json({ message: 'No token provided' });
   }
