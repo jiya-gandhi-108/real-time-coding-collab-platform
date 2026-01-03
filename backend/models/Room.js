fb35824ae52e2e1883621a726a7b61c2ebc74bdf
jiya-gandhi-108 <jiya108gandhi@gmail.com>
2026-01-03 12:25:18 +0530

    Update Room.js

--- backend/models/Room.js
+++ backend/models/Room.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
-  roomId: { type: String, required: true, unique: true },   // human friendly id
+  roomId: { type: String, required: true, unique: true },  
   passwordHash: { type: String, required: true },
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   createdAt: { type: Date, default: Date.now },
